1b8bc560e0f0dafa50ddaf905b3838076eab5e27
obaizou <baizouomar1@gmail.com>
2024-07-12 16:57:24 +0100

    remove

---
+++ lib/Views/screens/test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class test7 extends StatelessWidget {
+  const test7({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        //kanjrbo
+        );
+  }
+}
