9f1aace7b6c24e5ed4a6693fd93c7f11b7d477bd
obaizou <baizouomar1@gmail.com>
2024-07-18 02:20:39 +0100

    ylh bdit page ba9i masalitha payment

--- lib/Views/screens/payment.dart
+++ lib/Views/screens/payment.dart
@@ -1,18 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:trading_books/Core/Constants/AppColor.dart';
 import 'package:trading_books/Views/widgets/homeWidgets/BankCard.dart';
+import 'package:trading_books/Views/widgets/profilTextfield.dart';
 
 class Payment extends StatelessWidget {
   const Payment({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final screenwidth = mediaQuery.size.width;
+    final screenheight = mediaQuery.size.height;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Payment'),
         centerTitle: true,
       ),
       body: ListView(
-        children: [Bankcard()],
+        children: [
+          Bankcard(
+            fulname: "omar baizou",
+            year: "29",
+            month: "06",
+            CardNumber: "1234 1234 1234 1234",
+          ),
+          Container(
+              margin: EdgeInsets.symmetric(
+                  horizontal: screenwidth * 0.1, vertical: screenheight * 0.03),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(hintText: "Card Number"),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(hintText: "Card Number"),
+                        ),
+                      ),
+                      SizedBox(
+                        width: screenwidth * 0.1,
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(hintText: "Card Number"),
+                        ),
+                      ),
+                    ],
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(hintText: "Card Number"),
+                  ),
+                ],
+              )),
+          MaterialButton(
+              height: Get.height / 20,
+              color: AppColor.primarycolor,
+              child: const Text(
+                "Confirm",
+                style: TextStyle(color: AppColor.white, fontSize: 17),
+              ),
+              onPressed: () {}),
+        ],
       ),
     );
   }
--- lib/Views/widgets/homeWidgets/BankCard.dart
+++ lib/Views/widgets/homeWidgets/BankCard.dart
@@ -3,19 +3,27 @@ import 'package:trading_books/Core/Constants/AppColor.dart';
 import 'package:trading_books/Core/Constants/AppImages.dart';
 
 class Bankcard extends StatelessWidget {
-  const Bankcard({super.key});
-  // final String firstname;
+  const Bankcard(
+      {super.key,
+      required this.fulname,
+      required this.month,
+      required this.year,
+      required this.CardNumber});
+  final String fulname;
+  final String month;
+  final String year;
+  final String CardNumber;
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
     final screenHeight = mediaQuery.size.height;
     final screenWidth = mediaQuery.size.width;
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 55),
-      height: screenHeight * 0.2,
+      margin: const EdgeInsets.symmetric(horizontal: 55),
+      height: screenHeight * 0.22,
       decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [AppColor.black, const Color.fromARGB(255, 79, 73, 73)],
+          gradient: const LinearGradient(
+            colors: [AppColor.black, Color.fromARGB(255, 79, 73, 73)],
           ),
           borderRadius: BorderRadius.circular(20)),
       child: Column(
@@ -34,33 +42,25 @@ class Bankcard extends StatelessWidget {
           SizedBox(
             height: screenHeight * 0.07,
           ),
-          Row(
-            children: [
-              SizedBox(
-                width: screenWidth * 0.02,
-              ),
-              Text(
-                "Omar",
-                style: TextStyle(color: AppColor.white),
-              ),
-              SizedBox(
-                width: screenWidth * 0.01,
-              ),
-              Text(
-                "Baizou",
-                style: TextStyle(color: AppColor.white),
-              ),
-              SizedBox(
-                width: screenWidth * 0.3,
-              ),
-              Container(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "06/23",
-                  style: TextStyle(color: AppColor.white),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: screenWidth * 0.02),
+            child: Row(
+              children: [
+                Text(
+                  "$fulname",
+                  style: const TextStyle(color: AppColor.white),
                 ),
-              )
-            ],
+                SizedBox(
+                  width: screenWidth * 0.39,
+                ),
+                Text(
+                  "$month/$year",
+                  style: const TextStyle(
+                    color: AppColor.white,
+                  ),
+                )
+              ],
+            ),
           ),
           Row(
             children: [
@@ -68,8 +68,8 @@ class Bankcard extends StatelessWidget {
                 width: screenWidth * 0.02,
               ),
               Text(
-                "1234 5678 9123 4567",
-                style: TextStyle(color: AppColor.white),
+                CardNumber,
+                style: const TextStyle(color: AppColor.white),
               ),
             ],
           ),
